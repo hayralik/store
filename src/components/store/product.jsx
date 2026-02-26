a94eba8968f0f8be96da5afb3a967f7148d7d7f9
hayralik <hayralik@gmail.com>
2026-02-26 16:15:14 +0400

    CSS changed

--- src/components/store/product.jsx
+++ src/components/store/product.jsx
@@ -1,11 +1,12 @@
+import './product.css';
+
 function Product(props) {
   return (
-    <div style={{ border: '1px solid gray', margin: '10px', padding: '10px' }}>
+    <div className="product-card">   {/* ← вместо style */}
       <h3>{props.name}</h3>
       <p>Цена: {props.price} ₽</p>
       <button>В корзину</button>
     </div>
   );
 }
-
 export default Product;
--- src/components/store/productlist.jsx
+++ src/components/store/productlist.jsx
@@ -6,7 +6,11 @@ function ProductList() {
     { id: 1, name: "Ноутбук", price: 75000 },
     { id: 2, name: "Мышь", price: 1500 },
     { id: 3, name: "Клавиатура", price: 3500 },
-    { id: 4, name: "Монитор", price: 18000 }
+    { id: 4, name: "Монитор", price: 18000 },
+    { id: 5, name: "Ноутбук", price: 75000 },
+    { id: 6, name: "Мышь", price: 1500 },
+    { id: 7, name: "Клавиатура", price: 3500 },
+    { id: 8, name: "Монитор", price: 18000 }
   ];
 
   return (
