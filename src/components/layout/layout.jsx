30a582bc1a85b89a3baea3aae66ad14a0c9e27b4
hayralik <hayralik@gmail.com>
2026-05-10 10:03:29 +0400

    Update layout.jsx

--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -10,7 +10,7 @@ export default function Layout({ children }) {
       </header>
 
       <Navigation />
-      
+      Test
       <LoginForm onLoginSuccess={() => {
         // Например, скрыть форму входа или обновить состояние
         console.log('Вход выполнен');
